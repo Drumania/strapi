358c308be0cdd0e1565b3e7bb8adbf9ffe1845b2
martinbrumana <martinbrumana@gmail.com>
2023-09-05 21:37:39 -0300

    server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "secret"),
+      keys: [
+        "djjnhvEa1r9giP1URfp/bw==,ORl0aceuv8w6RZ2ExZlhXA==,JVTNvcomceaDAbV0cj8Tgw==,+C8onLL+g7rCsRdSc4epYg==",
+      ], // Agrega tus claves de aplicación aquí
+    },
   },
 });
